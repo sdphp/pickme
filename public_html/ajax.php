88475abc7939e290d828170125d4a25eab03fd84
Spencer Piontkowski <spencer.piontkowski@toastedlime.com>
2017-08-07 21:35:27 -0700

    Run the PhpStorm code formatter on all the relevant files

--- public_html/ajax.php
+++ public_html/ajax.php
@@ -3,10 +3,10 @@ require '../config.php';
 use \DMS\Service\Meetup\MeetupKeyAuthClient;
 
 $client = MeetupKeyAuthClient::factory(array('key' => $config['meetup_api']));
-$meetup_rsvps = $client->getRSVPs(array('event_id' => $_POST['event_id'] ));
+$meetup_rsvps = $client->getRSVPs(array('event_id' => $_POST['event_id']));
 
 $rsvps = array();
-foreach ($meetup_rsvps as $rsvp){
+foreach ($meetup_rsvps as $rsvp) {
     $rsvps[] = $rsvp;
 }
 
--- public_html/main.js
+++ public_html/main.js
@@ -10,7 +10,7 @@
  * @param eventid int The Meetup Event ID to be used for gathering a list.
  * @param callback function A function to execute after successful ajax request.
  */
-function get_attendees( eventid, callback ){
+function get_attendees(eventid, callback) {
 
     $.ajax({
         type: 'POST',
@@ -19,12 +19,12 @@ function get_attendees( eventid, callback ){
             event_id: eventid
         },
         success: callback,
-		dataType:'json'
+        dataType: 'json'
     })
-    .fail( function( xhr, status, error ){
-        $("#winner-container").html("There was an error with the ajax request. Check your internet connection and the console for more information.");
-        console.log("Status: " + status + " Error Thrown: " + error);
-    });
+        .fail(function (xhr, status, error) {
+            $("#winner-container").html("There was an error with the ajax request. Check your internet connection and the console for more information.");
+            console.log("Status: " + status + " Error Thrown: " + error);
+        });
 
 }
 
@@ -37,42 +37,42 @@ function get_attendees( eventid, callback ){
  * @return null
  */
 var attendee = [];
-function parse_attendees(attendees){
-        
+function parse_attendees(attendees) {
+
     // Check to see if we have results or an error.
-    
-    if(attendees){
-        
-        if( !isEmpty( attendees ) ) {
-          
-            for( i = 0; i < attendees.length; i++ ){
-                
-                if( attendees[i].response == "yes" ){
+
+    if (attendees) {
+
+        if (!isEmpty(attendees)) {
+
+            for (i = 0; i < attendees.length; i++) {
+
+                if (attendees[i].response == "yes") {
                     var obj = {};
                     obj.name = attendees[i].member.name;
                     attendees[i].member_photo ? obj.photo = attendees[i].member_photo.photo_link : obj.photo = "";
                     attendee.push(obj);
                 }
-            
+
             }
-            
-			select_winner( attendee );
-            
+
+            select_winner(attendee);
+
         }
-        
-        else{
-        
+
+        else {
+
             $("#winner-container").html("No RSVP's were found. Most likely, you didn't put in the right event ID. <a href=\"javascript:location.reload()\">Reload the page</a> and try again.");
-            
+
         }
-        
+
     }
-    
+
     else {
-        
+
         $("#winner-container").html(
-            data.status + "<p></p>"+
-            data.problem + "<p></p>"+
+            data.status + "<p></p>" +
+            data.problem + "<p></p>" +
             "Most likely, you didn't put in the right API key. <a href=\"javascript:location.reload()\">Reload the page</a> and try again."
         );
     }
@@ -84,12 +84,12 @@ function parse_attendees(attendees){
  * @param obj object The object in question.
  * @return boolean Returns true if the object is empty, false if not.
  */
-function isEmpty( obj ){
+function isEmpty(obj) {
 
-    for(var i in obj){
+    for (var i in obj) {
         return false;
     }
-    
+
     return true;
 
 }
@@ -100,55 +100,56 @@ function isEmpty( obj ){
  * @param rsvps mixed The RSVP's in the proper format.
  * @return null
  */
-function select_winner( rsvps ){
+function select_winner(rsvps) {
 
-    $('#winner-banner').text( 'Choosing winner in...' );
+    $('#winner-banner').text('Choosing winner in...');
 
 
-    var start_time = new Date().getTime(),
-        seconds_to_run = 5000, // in milliseconds
-        seconds = 0,
-        interval = setInterval( function(){
-        
-            seconds = new Date().getTime() - start_time;
-            
-            $('#countdown-timer').text( Math.round( Math.abs( ( seconds - seconds_to_run) / 1000 ) ) );
-            
-            if( seconds > seconds_to_run ){
-                
-                // If the code inside here runs, then display a picture of the chosen winner!
-                clearInterval(interval);
-                $('#winner-photo').html('<p><img src="' + rsvps[0].photo + '" width="150px" /></p><button id="again" class="btn btn-danger btn-lg">Run again?</button>&nbsp;<a href="javascript:location.reload();"><button class="btn btn-default btn-lg">Reload?</button></a>');
+    var start_time, seconds_to_run, seconds, interval;
+    start_time = new Date().getTime();
+    seconds_to_run = 5000;
+    seconds = 0;
+    interval = setInterval(function () {
+
+        seconds = new Date().getTime() - start_time;
+
+        $('#countdown-timer').text(Math.round(Math.abs((seconds - seconds_to_run) / 1000)));
+
+        if (seconds > seconds_to_run) {
+
+            // If the code inside here runs, then display a picture of the chosen winner!
+            clearInterval(interval);
+            $('#winner-photo').html('<p><img src="' + rsvps[0].photo + '" width="150px" /></p><button id="again" class="btn btn-danger btn-lg">Run again?</button>&nbsp;<a href="javascript:location.reload();"><button class="btn btn-default btn-lg">Reload?</button></a>');
+            $('#countdown-timer').empty();
+            $('#winner-banner').text('WINNER!!!').css({'font-size': '3em', 'color': 'blue', 'font-weight': 'bold'});
+
+            $('#again').click(function (e) {
+
+                $('#winner-banner').empty().css({'font-size': '', 'color': '', 'font-weight': ''});
                 $('#countdown-timer').empty();
-                $('#winner-banner').text('WINNER!!!').css( {'font-size':'3em','color':'blue','font-weight':'bold'} );
-	
-				$('#again').click(function(e){
-					
-					$('#winner-banner').empty().css( {'font-size':'','color':'','font-weight':''} );
-					$('#countdown-timer').empty();
-					$('#winner-name').empty();
-					$('#winner-photo').empty();
-					
-					select_winner(attendee);
-				});
-                
-                // Shoot off some fireworks.
-                var r = 4 + parseInt(Math.random()*16);
-                for(var i = r; i--;){
-                    setTimeout(
-                        'createFirework(8,14,2,null,null,null,null,null,Math.random()>0.5,true)',
-                        (i+1)*(1+parseInt(Math.random()*1000)));
-                    }
-                    
-                return; // End the program.
-                
-            }
-            
-            shuffle( rsvps );
-            
-            $('#winner-name').text( rsvps[0].name );
+                $('#winner-name').empty();
+                $('#winner-photo').empty();
 
-        }, 50 );
+                select_winner(attendee);
+            });
+
+            // Shoot off some fireworks.
+            var r = 4 + parseInt(Math.random() * 16);
+            for (var i = r; i--;) {
+                setTimeout(
+                    'createFirework(8,14,2,null,null,null,null,null,Math.random()>0.5,true)',
+                    (i + 1) * (1 + parseInt(Math.random() * 1000)));
+            }
+
+            return; // End the program.
+
+        }
+
+        shuffle(rsvps);
+
+        $('#winner-name').text(rsvps[0].name);
+
+    }, 50);
 
 }
 
@@ -158,23 +159,23 @@ function select_winner( rsvps ){
  * @param array array The array to be randomized.
  * @return array The randomized array.
  */
-function shuffle( array ) {
+function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue,
         randomIndex;
 
     // While elements remain...
-    while ( 0 !== currentIndex ) {
+    while (0 !== currentIndex) {
 
         // Pick one of the elements...
-        randomIndex = Math.floor( Math.random() * currentIndex );
+        randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
 
         // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
-        
+
     }
 
     return array;
@@ -183,15 +184,15 @@ function shuffle( array ) {
 /**
  * Finally, bind an event handler to the submit button click event.
  */
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#submit').click(function(e){
-        var event = $( '#meetingid' ).val();
-        
-        if( event ){
+    $('#submit').click(function (e) {
+        var event = $('#meetingid').val();
+
+        if (event) {
             $('#form-container').slideUp();
-            get_attendees( event, parse_attendees );
-        } 
+            get_attendees(event, parse_attendees);
+        }
     });
 
 });
